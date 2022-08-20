e97c4a25e542b91bbba2583350c7cbce7a6120a5
Pavel Montes <pavelmontes22@gmail.com>
2022-08-19 22:41:56 -0600

    hero has been added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const API = 'https://rickandmortyapi.com/api/character/1';
-const imgContainer =  document.getElementById('');
+const imgContainer1 =  document.getElementById('hero-img1');
+const imgContainer2 =  document.getElementById('hero-img2');
+const imgContainer3 =  document.getElementById('hero-img3');
+imgContainer1.alt = "rickImg";
 
-
+const setImg = (imagenContainer, url) => {
+    imagenContainer.src = url
+}
 
 fetch(API)
     .then(response => response.json())
-    .then(data =>  data.image);
+    .then(data =>  {
+        console.log(data.image)
+        // rickImg = data
+        setImg(imgContainer1, data.image)
+    });
 
 
 
