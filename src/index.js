7f70b73b646565dde89192bf1b73a54b0ae5f6e5
Pavel Montes <pavelmontes22@gmail.com>
2022-08-20 19:38:26 -0600

    Carrusel shows the API images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const API = 'https://rickandmortyapi.com/api/character/1';
+const API = 'https://rickandmortyapi.com/api/character';
 const imgContainer1 =  document.getElementById('hero-img1');
 const imgContainer2 =  document.getElementById('hero-img2');
 const imgContainer3 =  document.getElementById('hero-img3');
@@ -8,13 +8,40 @@ const setImg = (imagenContainer, url) => {
     imagenContainer.src = url
 }
 
-fetch(API)
-    .then(response => response.json())
+fetch(`${API}/1`)
+    .then(response => {
+        if (response.ok){
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    })
     .then(data =>  {
-        console.log(data.image)
-        // rickImg = data
-        setImg(imgContainer1, data.image)
+        setImg(imgContainer1, data.image);
+        return fetch(`${API}/2`);
+    })
+    .then(response => {
+        if (response.ok){
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    })
+    .then(data => {
+        setImg(imgContainer2, data.image)
+        return fetch(`${API}/3`)
+    })
+    .then(response => {
+        if (response.ok){
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    })
+    .then(data => {
+        setImg(imgContainer3, data.image)
     });
+
 
 
 
