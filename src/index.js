fad822fdcf0bb9d9d0527495ada79101cbc19a87
Pavel Montes <pavelmontes22@gmail.com>
2022-08-26 21:16:18 -0600

    Characters API call works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const API = 'https://rickandmortyapi.com/api/character';
-const imgContainer1 =  document.getElementById('hero-img1');
-const imgContainer2 =  document.getElementById('hero-img2');
-const imgContainer3 =  document.getElementById('hero-img3');
-imgContainer1.alt = "rickImg";
+
+const mainCharacters = {
+    mainCharacter1: document.getElementById('hero-img1'),
+    mainCharacter2: document.getElementById('hero-img2'),
+    mainCharacter3: document.getElementById('hero-img3'),
+
+}
+
+const characters = {
+    character1: document.getElementById('characters-img1'),
+    character2: document.getElementById('characters-img2'),
+    character3: document.getElementById('characters-img3'),
+    character4: document.getElementById('characters-img4'),
+    character5: document.getElementById('characters-img5'),
+    character6: document.getElementById('characters-img6'),
+}
+
+// const imgContainer1 =  document.getElementById('hero-img1');
+// const imgContainer2 =  document.getElementById('hero-img2');
+// const imgContainer3 =  document.getElementById('hero-img3');
+// imgContainer1.alt = "rickImg";
+console.log(mainCharacters)
+
 
 const setImg = (imagenContainer, url) => {
     imagenContainer.src = url
@@ -17,7 +36,7 @@ fetch(`${API}/1`)
         }
     })
     .then(data =>  {
-        setImg(imgContainer1, data.image);
+        setImg(mainCharacters.mainCharacter1, data.image);
         return fetch(`${API}/2`);
     })
     .then(response => {
@@ -28,7 +47,7 @@ fetch(`${API}/1`)
         }
     })
     .then(data => {
-        setImg(imgContainer2, data.image)
+        setImg(mainCharacters.mainCharacter2, data.image)
         return fetch(`${API}/3`)
     })
     .then(response => {
@@ -39,9 +58,41 @@ fetch(`${API}/1`)
         }
     })
     .then(data => {
-        setImg(imgContainer3, data.image)
-    });
+        setImg(mainCharacters.mainCharacter3, data.image)
+    })
+    .catch(error => console.error(error));
 
+fetch(`${API}/4`)
+    .then(response => response.ok ? response.json() : Promise.reject(response))
+    .then(data => {
+        setImg(characters.character1, data.image)
+        return fetch(`${API}/5`)
+    })
+    .then(response => response.ok ? response.json() : Promise.reject(response))
+    .then(data => {
+        setImg(characters.character2, data.image)
+        return fetch(`${API}/6`)
+    })
+    .then(response => response.ok ? response.json() : Promise.reject(response))
+    .then(data => {
+        setImg(characters.character3, data.image)
+        return fetch(`${API}/7`)
+    })
+    .then(response => response.ok ? response.json() : Promise.reject(response))
+    .then(data => {
+        setImg(characters.character4, data.image)
+        return fetch(`${API}/8`)
+    })
+    .then(response => response.ok ? response.json() : Promise.reject(response))
+    .then(data => {
+        setImg(characters.character5, data.image)
+        return fetch(`${API}/9`)
+    })
+    .then(response => response.ok ? response.json() : Promise.reject(response))
+    .then(data => {
+        setImg(characters.character6, data.image)
+    })
+    .catch(error => console.error(error));
 
 
 
