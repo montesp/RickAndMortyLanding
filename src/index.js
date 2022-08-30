16cc048e443674217fc991322e09cc1ac7e2aab1
Pavel Montes <pavelmontes22@gmail.com>
2022-08-29 21:49:34 -0600

    fetch changed, now use async/await instead

--- src/index.js
+++ src/index.js
@@ -1,98 +1,64 @@
 const API = 'https://rickandmortyapi.com/api/character';
-
-const mainCharacters = {
-    mainCharacter1: document.getElementById('hero-img1'),
-    mainCharacter2: document.getElementById('hero-img2'),
-    mainCharacter3: document.getElementById('hero-img3'),
-
-}
-
-const characters = {
-    character1: document.getElementById('characters-img1'),
-    character2: document.getElementById('characters-img2'),
-    character3: document.getElementById('characters-img3'),
-    character4: document.getElementById('characters-img4'),
-    character5: document.getElementById('characters-img5'),
-    character6: document.getElementById('characters-img6'),
-}
-
-// const imgContainer1 =  document.getElementById('hero-img1');
-// const imgContainer2 =  document.getElementById('hero-img2');
-// const imgContainer3 =  document.getElementById('hero-img3');
-// imgContainer1.alt = "rickImg";
-console.log(mainCharacters)
-
+const heroContainer = null || document.getElementById('hero-carrusel');
+const mainContainer = document.getElementById('characters-carrusel');
 
 const setImg = (imagenContainer, url) => {
     imagenContainer.src = url
 }
 
-fetch(`${API}/1`)
-    .then(response => {
-        if (response.ok){
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    })
-    .then(data =>  {
-        setImg(mainCharacters.mainCharacter1, data.image);
-        return fetch(`${API}/2`);
-    })
-    .then(response => {
-        if (response.ok){
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    })
-    .then(data => {
-        setImg(mainCharacters.mainCharacter2, data.image)
-        return fetch(`${API}/3`)
-    })
-    .then(response => {
-        if (response.ok){
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    })
-    .then(data => {
-        setImg(mainCharacters.mainCharacter3, data.image)
-    })
-    .catch(error => console.error(error));
+async function fetchData(api){
+    const response = await fetch(api);
 
-fetch(`${API}/4`)
-    .then(response => response.ok ? response.json() : Promise.reject(response))
-    .then(data => {
-        setImg(characters.character1, data.image)
-        return fetch(`${API}/5`)
-    })
-    .then(response => response.ok ? response.json() : Promise.reject(response))
-    .then(data => {
-        setImg(characters.character2, data.image)
-        return fetch(`${API}/6`)
-    })
-    .then(response => response.ok ? response.json() : Promise.reject(response))
-    .then(data => {
-        setImg(characters.character3, data.image)
-        return fetch(`${API}/7`)
-    })
-    .then(response => response.ok ? response.json() : Promise.reject(response))
-    .then(data => {
-        setImg(characters.character4, data.image)
-        return fetch(`${API}/8`)
-    })
-    .then(response => response.ok ? response.json() : Promise.reject(response))
-    .then(data => {
-        setImg(characters.character5, data.image)
-        return fetch(`${API}/9`)
-    })
-    .then(response => response.ok ? response.json() : Promise.reject(response))
-    .then(data => {
-        setImg(characters.character6, data.image)
-    })
-    .catch(error => console.error(error));
+    if (response.ok){
+        const data = await response.json();
+        return data.image;
+    } else {
+        return Promise.reject(response);
+    }
+}
 
 
+const heroImagesContainerView = async (images, container) => {
+    let view = "";
+    for (let index = 0; index < images.length; index++) {
+        view += `
+            <img class="carrusel-hero__image" src="${images[index]}" alt="">
+        `;
+        container.innerHTML = view;
+
+    }
+}
+
+const mainCharacterImageContainer = async (images, container) => {
+    let view = "";
+    for (let index = 0; index < images.length; index++) {
+        view += `
+            <img class="carrusel-character__image" src="${images[index]}" alt="">
+        `;
+        container.innerHTML = view;
+
+    }
+}
+
+
+
+(async () => {
+    try {
+        let images = []
+        for (let i = 1; i <= 3; i++) {
+            images.push(await fetchData(`${API}/${i}`))
+        }
+        await heroImagesContainerView(images, heroContainer)
+
+        images = []
+        for (let i = 4; i < 12; i++) {
+            images.push(await fetchData(`${API}/${i}`))
+        }
+        console.log(images)
+        await mainCharacterImageContainer(images, mainContainer);
+
+    } catch (err) {
+        console.log(err)
+    }
+})();
 
